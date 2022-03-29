7832f028213dfc61763b472313098c9092eca3f6
Kausthubh Sukhesha Adhikari <kausthubhadhikari@gmail.com>
2022-03-29 14:55:40 +0530

    Initial Commit

---
+++ VideoPlayerTestNewUITests/VideoPlayerTestNewUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  VideoPlayerTestNewUITestsLaunchTests.swift
+//  VideoPlayerTestNewUITests
+//
+//  Created by Kausthubh adhikari on 29/03/22.
+//
+
+import XCTest
+
+class VideoPlayerTestNewUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
